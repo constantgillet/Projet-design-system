cf434d6d81e18f8ba591f8756063dd08e2d12fef
constantgillet <constant.gillet@hetic.net>
2020-10-27 11:16:47 +0100

    🎨 Add color variations

--- client/src/js/pages/LoginPage.js
+++ client/src/js/pages/LoginPage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 export default function LoginPage() {
+
+    //Component did mount
+    useEffect(() => {
+        document.title = 'Login'
+    }, [])
+
     return (
         <div>
             <h1>Login page</h1>
